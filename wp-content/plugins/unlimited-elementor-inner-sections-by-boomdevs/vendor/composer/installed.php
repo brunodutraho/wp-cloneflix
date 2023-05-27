7e437fb6a27f72964640beb9541ffd3e0f05d9b3
Bruno Dutra <122378239+brunodutraho@users.noreply.github.com>
2023-05-27 11:18:32 -0300

    Primeiro Commit

---
+++ wp-content/plugins/unlimited-elementor-inner-sections-by-boomdevs/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'boom-devs/elementor-unlimited-inner-sections',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'appsero/client' => array(
+            'pretty_version' => 'v1.2.2',
+            'version' => '1.2.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../appsero/client',
+            'aliases' => array(),
+            'reference' => 'fba140030f7141ed71eeba192b5437b2f30d81a8',
+            'dev_requirement' => false,
+        ),
+        'boom-devs/elementor-unlimited-inner-sections' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
